d030335af04165eb4ce8e23107513d536d7bf408
Ayamgorng <96842816+Ayamgorng@users.noreply.github.com>
2025-02-23 23:17:05 +0700

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,3 +1,3 @@
-const supabaseUrl = process.env.PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = process.env.PUBLIC_SUPABASE_KEY as string
+const supabaseUrl = process.env.SUPABASE_URL as string
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
